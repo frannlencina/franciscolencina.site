419865838c46bf7998243fbab36535ac3055a419
Francisco Lencina <franciscolencinap@gmail.com>
2023-03-04 05:19:50 -0300

    second commit

---
+++ src/components/DetailsButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function DetailsButton() {
+    return (
+        <div>
+            <div
+                class="flex text-center justify-center p-2 bg-[var(--blue)] rounded-md text-white hover:text-blue-400 duration-300 cursor-pointer"
+            >
+                <button class="">Ver Detalles</button>
+            </div>
+        </div>
+    )
+}
+
+export default DetailsButton
---
+++ src/components/DvButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Curriculum from '/curriculum.pdf'
+
+function DvButton() {
+    return (
+        <div className='hover:scale-105 transition duration-400'>
+            <a
+                className="cvButton flex items-center gap-2 px-5 py-4 rounded-xl bg-[#344FA0] font-medium text-white sm:px-6 sm:py-3 sm:rounded-lg "
+                href={Curriculum} download
+            >Descargar CV <i class="ri-file-download-line"></i>
+            </a>
+
+        </div>
+    )
+}
+
+export default DvButton
