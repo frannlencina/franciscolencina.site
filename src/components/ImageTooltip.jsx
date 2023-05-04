8b20c2830aa15cb2f958920a1fe062d0e98c2758
Francisco Lencina <franciscolencinap@gmail.com>
2023-05-04 16:47:51 -0300

    fixing tippyjs error

--- src/components/ImageTooltip.jsx
+++ src/components/ImageTooltip.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 
-function ImageTooltip({ name, image }) {
+function ImageTooltip(props) {
 
     return (
-        <Tippy content={name}>
-            <img className='hover:scale-105  w-20 md:w-full' src={image} alt={name} />
+        <Tippy content={props.name}>
+            <img className='hover:scale-105  w-20 md:w-full' src={props.image} alt={props.name} />
         </Tippy>
     )
 }
