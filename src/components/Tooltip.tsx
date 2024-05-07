610c94c9b6bac1e6a571091d5d58c25cf6334219
Francisco <cinkii@protonmail.com>
2024-05-07 12:31:49 -0300

    fix 🐛: fixing white background with view transition

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,6 +1,4 @@
-
 export default function Tooltip({ text }) {
-
     return (
         <div className="flex flex-col justify-center items-center select-none">
             <div className=" relative z-30 px-3 py-1 text-md bg-[#3E3E3E] text-[#8B8B8B] font-bold border-[#4F4F4F] border-2 rounded-lg hover:ring-2 hover:ring-[#797979] transition-all duration-200">
