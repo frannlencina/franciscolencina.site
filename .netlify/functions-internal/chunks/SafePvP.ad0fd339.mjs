177fcd9045def964f081314766a7adea5ff8a422
Francisco <cinkii@protonmail.com>
2024-05-29 00:23:50 -0300

    fix 🐛: netlify cache

--- .netlify/functions-internal/chunks/SafePvP.ad0fd339.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./SafePvP.e71afae2.mjs');
-					}
-					const collectedLinks = ["/_astro/Post.astro_astro_type_style_index_0_lang.4f950d41.css"];
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = [{"props":{"src":"/_astro/ViewTransitions.astro_astro_type_script_index_0_lang.b6f7a4c6.js","type":"module"},"children":""}];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
